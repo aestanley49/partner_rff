29da465e8a96feb10dac78abe6a85c2aafe2fc59
aestanley49 <aestanley49@gmail.com>
2020-08-06 14:25:10 -0400

    Added relevant consultants to cooperators column Those commented out already had organization in list of partners

--- functions/FormatwCollabs.R
+++ functions/FormatwCollabs.R
@@ -19,14 +19,25 @@
 
 FormatwCollabs <- function(PartnersDataModified){ 
   
-  ## 1. Remove columns with extra information 
+  ## 1a. Remove columns with extra information 
   PartnersDataModified <- PartnersDataModified[,-c(1, 4,5, 11:15)]
   
-  ## 1.5 Need to remove comma at end of Flat-tailed horned lizard or have comma issue 
+  ## 1b. Need to remove comma at end of Flat-tailed horned lizard or have comma issue 
   
   PartnersDataModified[which(PartnersDataModified$common_name =="FLAT-TAILED HORNED LIZARD"),5] <- "California Department of Parks and Recreation Anza Borrego Desert State Park, BLM El Centro, BLM Palm Springs, BLM Yuma District, BLM California Desert District, Bureau of Reclamation Yuma, USFWS Carlsbad Field Office, USFWS Phoenix Field Office, US Marine Corps Air Station Yuma, US Naval Air Facility-El Centro, US Navy SW Division San Diego, Arizona Game and Fish, California Department of Fish and Game, California Department of Parks and Recreation Off-Highway Motor Vehicle Division Ocotillo Wells State Recreational Vehicle Area, California Department of Parks and Recreation Anza Borrego Desert State Park"
   PartnersDataModified[which(PartnersDataModified$common_name =="FLAT-TAILED HORNED LIZARD"),6] <- "California Department of Parks and Recreation Anza Borrego Desert State Park, BLM, Bureau of Reclamation, USFWS, US Marine Corps, US Navy, Arizona Game and Fish, California Department of Fish and Game, California Department of Parks and Recreation Off-Highway Motor Vehicle Division Ocotillo Wells State Recreational Vehicle Area, California Department of Parks and Recreation Anza Borrego Desert State Park"
 
+  
+  ## 1c. Add "prepared by" consultants with interest in species to collaborators columns for species 
+  
+  PartnersDataModified[which(PartnersDataModified$common_name =="ARCTIC GRAYLING- UPPER MISSOURI RIVER DPS"),7] ##BLM Dillon Feild Office listed under "prepared by" but already counted in code 
+  PartnersDataModified[which(PartnersDataModified$common_name =="CORAL PINK SAND DUNES TIGER BEETLE"),7] ##Conservation Committe for the Coral Pink Sand Dunes Tiger Beetle - not added because this is a group of multiple entities not a single partner
+  PartnersDataModified[which(PartnersDataModified$common_name =="CUYAMAC LAKE DOWNINGIA"),7] <- "Department of Biology San Diego State University"
+  PartnersDataModified[which(PartnersDataModified$common_name =="LEAST CHUB"),7] <- "Utah Department of Natural Resources"
+##PartnersDataModified[which(PartnersDataModified$common_name =="NEVARES SPRING NAUCORID BUG (=FURNACE CREEK)"),7] <- "Desert Reserach Institute, NPS Death Valley National Park" --> already included in partner list
+##PartnersDataModified[which(PartnersDataModified$common_name == "NEW ENGLAND COTTONTAIL"),7] <- "Wildlife Management Institute, USFWS, the New England Cottontail Technical Committee" --> already included in partner list
+  PartnersDataModified[which(PartnersDataModified$common_name =="ORCUTTS'S HAZARDIA"),7] <- "Center for Natural Lands Management"
+##PartnersDataModified[which(PartnersDataModified$common_name =="WASHINGTON GROUND SQUIRREL"),7] <- "Foster Creek Conservation District" --> already included in partner list
 
   # 2. Counts for each of the new columns 
     #Column with federal agency offices added 
